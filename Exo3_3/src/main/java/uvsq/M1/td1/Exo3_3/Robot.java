7af54483fedc1ad92a39e94b9b1ed648bfd0f9ff
Rachida <rachidaouchene1@gmail.com>
2020-03-05 00:15:32 +0100

    implimantation de Robot

--- Exo3_3/src/main/java/uvsq/M1/td1/Exo3_3/Robot.java
+++ Exo3_3/src/main/java/uvsq/M1/td1/Exo3_3/Robot.java
@@ -2,5 +2,13 @@ package uvsq.M1.td1.Exo3_3;
 
 public class Robot {
 	private Position position ;
-	private Direction direction ;  
+	private Direction direction ; 
+public Robot()
+{
+	}
+public void tourne()
+{
+	
+}
+
 }
--- Exo3_3/src/main/java/uvsq/M1/td1/Exo3_3/RobotMobile.java
+++ Exo3_3/src/main/java/uvsq/M1/td1/Exo3_3/RobotMobile.java
@@ -2,4 +2,6 @@ package uvsq.M1.td1.Exo3_3;
 
 public class RobotMobile extends Robot {
 
+	
+	public void avance() {}
 }
